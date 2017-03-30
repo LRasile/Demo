3438184a08497b7c307d06fd446074dd748048cc
LRasile <rasile.leonardo@gmail.com>
2017-03-30 22:26:13 +0100

    Table is being bound and added Int32 in to the list

--- DemoFramework/Models/ProductViewModel.cs
+++ DemoFramework/Models/ProductViewModel.cs
@@ -5,16 +5,13 @@ namespace DemoFramework.Models
 {
     public class ProductViewModel
     {
-        [Required]
-        [Range(0,Int32.MaxValue)]
+        [Required, Range(0,Int32.MaxValue)]
         public int Id { get; set; }
 
         [Required]
         public string Name { get; set; }
 
-        [Required]
-        [RegularExpression("^\\d*(\\.\\d{2})$")]
-        [DisplayFormat(DataFormatString = "{0:n2}", ApplyFormatInEditMode = true)]
-        public double Price { get; set; }
+        [Required, DataType(DataType.Currency)]
+        public decimal Price { get; set; }
     }
 }
